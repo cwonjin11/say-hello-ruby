26da5db626f781b58057bb6b12b3da4bdff2d081
Wonjin <cwonjin1@gmail.com>
2020-12-20 13:53:47 -0500

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,15 +1,14 @@
 def say_hello(name)
     puts "Hello #{name}!"
-  end
+end
   
-  say_hello("Kent Beck")
+say_hello("Kent Beck")
   
   
-  def say_hello(name = "Ruby Programmer")
+def say_hello(name = "Ruby Programmer")
     puts "Hello #{name}!"
-  end
+end
   
-
 say_hello
 
 
